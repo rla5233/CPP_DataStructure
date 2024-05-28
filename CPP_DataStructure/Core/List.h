c6c971f45a051ac03942eb0487ec1996178a0726
김수원 <rla5233@naver.com>
2024-05-28 16:06:21 +0900

    김수원 : queue 시작

--- CPP_DataStructure/Core/List.h
+++ CPP_DataStructure/Core/List.h
@@ -62,17 +62,10 @@ namespace ksw
 		};
 
 	public:
-		// constructor destructor
 		list();
 		list(size_t _Size);
 		list(Type _Data, size_t _Size);
 		~list();
-
-		// delete Function
-		list(const list& _Other) = delete;
-		list(list&& _Other) noexcept = delete;
-		list& operator=(const list& _Other) = delete;
-		list& operator=(list&& _Other) noexcept = delete;
 
 	public:
 		inline Type front();
---
+++ CPP_DataStructure/Core/Queue.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace ksw
+{
+	template<typename Type>
+	class queue
+	{
+		queue() {};
+		~queue() {};
+
+	};
+}
